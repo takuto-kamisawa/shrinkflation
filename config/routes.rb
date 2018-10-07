2d45461e18892412e64e96c2de2a5881881b4a22
takuto-kamisawa <takuto.kamisawa+github@gmail.com>
2018-10-08 06:47:33 +0900

    home#detailの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'products/index'
   get 'home/index'
+  get 'home/detail'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
