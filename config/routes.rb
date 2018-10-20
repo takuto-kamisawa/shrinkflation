82ec036914ac128ff1cfa0741ab8f39736a2dab7
takuto-kamisawa <takuto.kamisawa+github@gmail.com>
2018-10-21 03:46:57 +0900

    route・productを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index'
+  root "products#index"
   get 'home/index'
   get 'home/detail'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
