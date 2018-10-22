39340a9e6633c2d97e5008fb9c65c8c85d7d7ab5
EC2 Default User <ec2-user@ip-172-31-10-248.ap-southeast-1.compute.internal>
2018-10-22 07:50:41 +0000

    newページ用メソッド追加

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,16 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+  
+   def new
+     @product = Product.new
+　 end
+　 
+　 def create
+  @product = Product.new(params.require(:product).permit(:maker, :name, :NET_weight, :price, :priceperweight))
+  @product.save
+  redirect_to products_path
+　 end
 end
 
 #<% select :name, :maker, :NET_weight, :price, :priceperweight  %>
