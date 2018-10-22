74aebbf3838b4fd2278568e5ec8fe2d240988423
EC2 Default User <ec2-user@ip-172-31-10-248.ap-southeast-1.compute.internal>
2018-10-22 01:24:21 +0000

    newページ用routeとviewsファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "products#index"
   get 'home/index'
   get 'home/detail'
+  get 'products/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
