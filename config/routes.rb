c5dcf321bc6fa732a95ccc0698c3956509afdfa5
EC2 Default User <ec2-user@ip-172-31-10-248.ap-southeast-1.compute.internal>
2018-10-24 03:07:19 +0000

    routesに記載のURLをresourcesに置き換え

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root "products#index"
+  resources :products
+  
   get 'home/index'
   get 'home/detail'
-  get 'products/new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
