d69c0e85f57d4adabc9f3b4ce5a4fec426727894
EC2 Default User <ec2-user@ip-172-31-37-158.ap-southeast-1.compute.internal>
2018-11-11 05:15:22 +0000

    column priceperweight削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_03_051841) do
+ActiveRecord::Schema.define(version: 2018_11_11_051326) do
 
   create_table "makers", force: :cascade do |t|
     t.string "name"
@@ -23,7 +23,6 @@ ActiveRecord::Schema.define(version: 2018_11_03_051841) do
     t.integer "maker_id"
     t.integer "NET_weight"
     t.integer "price"
-    t.float "priceperweight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
