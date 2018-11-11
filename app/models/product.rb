5bb6db2a7fae9705533acc0777b9436f5ac9bd0f
EC2 Default User <ec2-user@ip-172-31-37-158.ap-southeast-1.compute.internal>
2018-11-11 05:04:28 +0000

    before lesson 181111

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,24 @@
 class Product < ApplicationRecord
     belongs_to :maker
+    
+     def priceperweight
+#     price / NET_weight
+    #   before_create do
+    #     self.priceperweight = price / NET_weight
+    #   end
+    #  end
+
+
+
+
+
+#   def set_extra_information
+#   {priceperweight: priceperweight}
+#   end
+  
+#これだけで良さそう？
+ #before_save do
+     #self.priceperweight = price / NET_weight
+ #end
+     end
 end
